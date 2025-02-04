2b742c5a924ef79ad3e805f8de570477fbdb5236
sahil <sahilofficial17@gmail.com>
2025-02-03 23:43:14 -0800

    Set up Dockerfiles and added docker compose to build and run this app locally

--- backend/index.js
+++ backend/index.js
@@ -5,9 +5,9 @@ import cors from "cors"
 const app = express();
 
 const db = mysql.createConnection({
-    host: "localhost",
+    host: "db",
     user: "root",
-    password: "",
+    password: "my-secret-pw",
     database: "test"
 })
 
